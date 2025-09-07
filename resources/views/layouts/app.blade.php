6950476458698375e12415252f85b0e4b199e768
windi fikriyansyah <diwin321@gmail.com>
2025-09-07 21:30:53 +0700

    fitur baru

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>KelasSatu - Platform E-Course Terbaik</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.PNG') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <script src="https://cdn.tailwindcss.com"></script>
